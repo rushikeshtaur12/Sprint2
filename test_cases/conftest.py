6262af8f594d7ea8f15d3d7c94c5096f0b3aed28
Rushikesh12 <rushikeshtaur12@gmail.com>
2022-11-29 17:32:13 +0530

    commited

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -5,9 +5,15 @@ from librery.config import Config
 from webdriver_manager.microsoft import EdgeChromiumDriverManager # cross browsing
 from webdriver_manager.firefox import GeckoDriverManager
 
-@pytest.fixture
-def _driver():
-    driver_obj = webdriver.Chrome(ChromeDriverManager().install())
+@pytest.fixture(params=["Chrome","Safari"])
+def _driver(request):
+    if request.param == "Chrome":
+        driver_obj = webdriver.Chrome(ChromeDriverManager().install())
+
+    elif request.param == "Safari":
+        driver_obj = webdriver.Safari()
+
+
     driver_obj.get(Config.URL)
     driver_obj.implicitly_wait(30)
     driver_obj.maximize_window()
